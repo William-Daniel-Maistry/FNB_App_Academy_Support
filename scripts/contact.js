365a23f0fdd315804fb571beee5370b598694599
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2025-05-23 13:59:12 +0200

    Add files via upload

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,7 +3,6 @@ function openGmail() {
   const additionalName = document.querySelector("#additional-name").value;
   const lastName = document.querySelector("#lastname").value;
   const email = document.querySelector("#email").value;
-  const emailDomain = email.split("@")[1].toLowerCase();
   const phone = document.querySelector("#phone").value;
   const contactPreference = document.querySelector("#contact-preference").value;
   const messageType = document.querySelector("#message-type").value;
@@ -16,22 +15,7 @@ function openGmail() {
     `Hello FNB App Academy,\n\n${message}\n\nKind Regards,\n${firstName} ${additionalName} ${lastName}`
   );
 
-  let emailURL = "";
-
-  if (emailDomain.includes("gmail.com")) {
-    emailURL = `https://mail.google.com/mail/?view=cm&fs=1&to=fnbappacademyinfo@gmail.com&su=${subject}&body=${body}`;
-  } else if (
-    emailDomain.includes("outlook.com") ||
-    emailDomain.includes("hotmail.com") ||
-    emailDomain.includes("live.com")
-  ) {
-    emailURL = `https://outlook.live.com/owa/?path=/mail/action/compose&to=fnbappacademyinfo@gmail.com&subject=${subject}&body=${body}`;
-  } else if (emailDomain.includes("yahoo.com")) {
-    emailURL = `https://compose.mail.yahoo.com/?to=fnbappacademyinfo@gmail.com&subject=${subject}&body=${body}`;
-  } else {
-    // Default to mailto link
-    emailURL = `mailto:fnbappacademyinfo@gmail.com?subject=${subject}&body=${body}`;
-  }
+  const emailURL = `mailto:fnbappacademyinfo@gmail.com?subject=${subject}&body=${body}`;
 
   window.open(emailURL, "_blank");
 }
